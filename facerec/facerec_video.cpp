3a3954091c9306b5541e36bbcaee058d611b740b
Taylor Bartlett <taybart@gmail.com>
2015-02-12 11:01:32 -0700

    added msp430 and pru codes

--- facerec/facerec_video.cpp
+++ facerec/facerec_video.cpp
@@ -76,7 +76,7 @@ static int add_user(const char* userName, string fn_lbp, int deviceId){
         // Holds the current frame from the Video device:
         printf("Taking pictures of you %s so I can recognize your face MUWHAHAHAHA...\n",userName);
         Mat frame;
-        for(int j = 0;j<15;j++) {
+        for(int j = 0;j<10;j++) {
             cap >> frame;
             // Clone the current frame:
             Mat original = frame.clone();
@@ -101,7 +101,7 @@ static int add_user(const char* userName, string fn_lbp, int deviceId){
                 sprintf(imName,"faces/%s/%s%d.jpg",userName,userName,j);
                 imwrite(imName,face_resized);
                 // printf("Shift face slightly");
-                sleep(1);
+                // sleep(1);
 
             }
         }
@@ -119,113 +119,118 @@ int main(int argc, const char *argv[]) {
     string fn_lbp = string("lbpcascades/lbpcascade_frontalface.xml");
     string fn_csv = string("faces.csv");
     int deviceId = atoi("video0");
+    if (argc == 2) {
     // map<int, string> users;
     char newUseCh = 'n';
     printf("Add new user? (y/n): ");
     scanf(" %c",&newUseCh);
 
-    if(newUseCh=='y'){
-        char newUser[100];
-        printf("Username: ");
-        scanf("%s",newUser);
-        add_user(newUser,fn_lbp,deviceId);
-    }
-    // These vectors hold the images and corresponding labels:
-    vector<Mat> images;
-    vector<int> labels;
-    // Read in the data (fails if no valid input filename is given, but you'll get an error message):
-    try {
-        read_csv(fn_csv, images, labels);
-    } catch (cv::Exception& e) {
-        cerr << "Error opening file \"" << fn_csv << "\". Reason: " << e.msg << endl;
-        // nothing more we can do
-        exit(1);
-    }
-    // Get the height from the first image. We'll need this
-    // later in code to reshape the images to their original
-    // size AND we need to reshape incoming faces to this size:
-    int im_width = images[0].cols;
-    int im_height = images[0].rows;
-    // Create a FaceRecognizer and train it on the given images:
-    Ptr<FaceRecognizer> model = createLBPHFaceRecognizer();
-    model->train(images, labels);
-    // Ptr<FaceRecognizer> model = createEigenFaceRecognizer();
-    // Ptr<FaceRecognizer> model = createFisherFaceRecognizer();
-    // That's it for learning the Face Recognition model. You now
-    // need to create the classifier for the task of Face Detection.
-    // We are going to use the haar cascade you have specified in the
-    // command line arguments:
-    //
-    // CascadeClassifier haar_cascade;
-    // haar_cascade.load(fn_haar);
-
-    CascadeClassifier lbp_cascade;
-    lbp_cascade.load(fn_lbp);
-    // Get a handle to the Video device:
-    VideoCapture cap(deviceId);
-    // Check if we can use this device at all:
-    if(!cap.isOpened()) {
-        cerr << "Capture Device ID " << deviceId << "cannot be opened." << endl;
-        return -1;
-    }
-    // Holds the current frame from the Video device:
-    Mat frame;
-    for(;;) {
-        cap >> frame;
-        // Clone the current frame:
-        Mat original = frame.clone();
-        // Convert the current frame to grayscale:
-        Mat gray;
-        cvtColor(original, gray, CV_BGR2GRAY);
-        // Find the faces in the frame:
-        vector< Rect_<int> > faces;
-        lbp_cascade.detectMultiScale(gray, faces);
-        // haar_cascade.detectMultiScale(gray, faces);
-        // At this point you have the position of the faces in
-        // faces. Now we'll get the faces, make a prediction and
-        // annotate it in the video. Cool or what?
-        for(int i = 0; i < faces.size(); i++) {
-            // Process face by face:
-            Rect face_i = faces[i];
-            // Crop the face from the image. So simple with OpenCV C++:
-            Mat face = gray(face_i);
-            // Resizing the face is necessary for Eigenfaces and Fisherfaces. You can easily
-            // verify this, by reading through the face recognition tutorial coming with OpenCV.
-            // Resizing IS NOT NEEDED for Local Binary Patterns Histograms, so preparing the
-            // input data really depends on the algorithm used.
-            //
-            // I strongly encourage you to play around with the algorithms. See which work best
-            // in your scenario, LBPH should always be a contender for robust face recognition.
-            //
-            // Since I am showing the Fisherfaces algorithm here, I also show how to resize the
-            // face you have just found:
-            Mat face_resized;
-            cv::resize(face, face_resized, Size(im_width, im_height), 1.0, 1.0, INTER_CUBIC);
-            // Now perform the prediction, see how easy that is:
-            int prediction = model->predict(face_resized);
-
-            // And finally write all we've found out to the original image!
-            // First of all draw a green rectangle around the detected face:
-            rectangle(original, face_i, CV_RGB(150,150,150), 1);
-
-            // Create the text we will annotate the box with:
-            string box_text = format("User %d", prediction);
-
-            // Calculate the position for annotated text (make sure we don't
-            // put illegal values in there):
-            int pos_x = std::max(face_i.tl().x - 10, 0);
-            int pos_y = std::max(face_i.tl().y - 10, 0);
-
-            // And now put it into the image:
-            putText(original, box_text, Point(pos_x, pos_y), FONT_HERSHEY_PLAIN, 1.0, CV_RGB(150,150,150), 2.0);
+        if(newUseCh=='y'){
+            char newUser[100];
+            printf("Username: ");
+            scanf("%s",newUser);
+            add_user(newUser,fn_lbp,deviceId);
+            return 0;
         }
-        // Show the result:
-        imshow("face_recognizer", original);
-        // And display it:
-        char key = (char) waitKey(20);
-        // Exit this loop on escape:
-        if(key == 27)
-            break;
+    }
+    else{
+        // These vectors hold the images and corresponding labels:
+        vector<Mat> images;
+        vector<int> labels;
+        // Read in the data (fails if no valid input filename is given, but you'll get an error message):
+        try {
+            read_csv(fn_csv, images, labels);
+        } catch (cv::Exception& e) {
+            cerr << "Error opening file \"" << fn_csv << "\". Reason: " << e.msg << endl;
+            // nothing more we can do
+            exit(1);
+        }
+        // Get the height from the first image. We'll need this
+        // later in code to reshape the images to their original
+        // size AND we need to reshape incoming faces to this size:
+        int im_width = images[0].cols;
+        int im_height = images[0].rows;
+        // Create a FaceRecognizer and train it on the given images:
+        Ptr<FaceRecognizer> model = createLBPHFaceRecognizer();
+        model->train(images, labels);
+        // Ptr<FaceRecognizer> model = createEigenFaceRecognizer();
+        // Ptr<FaceRecognizer> model = createFisherFaceRecognizer();
+        // That's it for learning the Face Recognition model. You now
+        // need to create the classifier for the task of Face Detection.
+        // We are going to use the haar cascade you have specified in the
+        // command line arguments:
+        //
+        // CascadeClassifier haar_cascade;
+        // haar_cascade.load(fn_haar);
+
+        CascadeClassifier lbp_cascade;
+        lbp_cascade.load(fn_lbp);
+        // Get a handle to the Video device:
+        VideoCapture cap(deviceId);
+        // Check if we can use this device at all:
+        if(!cap.isOpened()) {
+            cerr << "Capture Device ID " << deviceId << "cannot be opened." << endl;
+            return -1;
+        }
+        // Holds the current frame from the Video device:
+        Mat frame;
+        for(;;) {
+            cap >> frame;
+            // Clone the current frame:
+            Mat original = frame.clone();
+            // Convert the current frame to grayscale:
+            Mat gray;
+            cvtColor(original, gray, CV_BGR2GRAY);
+            // Find the faces in the frame:
+            vector< Rect_<int> > faces;
+            lbp_cascade.detectMultiScale(gray, faces);
+            // haar_cascade.detectMultiScale(gray, faces);
+            // At this point you have the position of the faces in
+            // faces. Now we'll get the faces, make a prediction and
+            // annotate it in the video. Cool or what?
+            for(int i = 0; i < faces.size(); i++) {
+                // Process face by face:
+                Rect face_i = faces[i];
+                // Crop the face from the image. So simple with OpenCV C++:
+                Mat face = gray(face_i);
+                // Resizing the face is necessary for Eigenfaces and Fisherfaces. You can easily
+                // verify this, by reading through the face recognition tutorial coming with OpenCV.
+                // Resizing IS NOT NEEDED for Local Binary Patterns Histograms, so preparing the
+                // input data really depends on the algorithm used.
+                //
+                // I strongly encourage you to play around with the algorithms. See which work best
+                // in your scenario, LBPH should always be a contender for robust face recognition.
+                //
+                // Since I am showing the Fisherfaces algorithm here, I also show how to resize the
+                // face you have just found:
+                Mat face_resized;
+                cv::resize(face, face_resized, Size(im_width, im_height), 1.0, 1.0, INTER_CUBIC);
+                // Now perform the prediction, see how easy that is:
+                int prediction = model->predict(face_resized);
+
+                // And finally write all we've found out to the original image!
+                // First of all draw a green rectangle around the detected face:
+                rectangle(original, face_i, CV_RGB(150,150,150), 1);
+
+                // Create the text we will annotate the box with:
+                string box_text = format("User %d", prediction);
+
+                // Calculate the position for annotated text (make sure we don't
+                // put illegal values in there):
+                int pos_x = std::max(face_i.tl().x - 10, 0);
+                int pos_y = std::max(face_i.tl().y - 10, 0);
+
+                // And now put it into the image:
+                putText(original, box_text, Point(pos_x, pos_y), FONT_HERSHEY_PLAIN, 1.0, CV_RGB(150,150,150), 2.0);
+            }
+            // Show the result:
+            imshow("face_recognizer", original);
+            // And display it:
+            char key = (char) waitKey(20);
+            // Exit this loop on escape:
+            if(key == 27)
+                break;
+        }
     }
     return 0;
 }
