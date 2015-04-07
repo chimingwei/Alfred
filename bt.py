b5ce092a4e57ee90a900eecd7b0bba48dbdd4782
Taylor Bartlett <taybart@gmail.com>
2015-04-07 15:53:41 -0600

    initial bluetooth script

--- bt.py
+++ bt.py
@@ -1,11 +1,16 @@
-#import Adafruit_BBIO.UART as UART
-#import serial
+import Adafruit_BBIO.UART as UART
+import serial
 UART.setup("UART1")
 
-ser = serial.Serial(port = "/dev/ttyO1", baudrate=9600)
+ser = serial.Serial(port="/dev/ttyO1", baudrate=9600)
 ser.close()
 ser.open()
 while True:
     if ser.isOpen():
-       ser.read(12)
+        weatherInfo = ser.read(12)
+        weatherInfo.split(':')
+        weatherJsonResponse = "{\"humidity\":"+weatherInfo[0]
++",\"temp1\":"+weatherInfo[1]
++",\"temp2\":"+weatherInfo[2]
++",\"pressure\":"+weatherInfo[3]+"}"
 ser.close()
