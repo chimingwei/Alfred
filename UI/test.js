434c32fae5db08dc3b826ee8d702e7dc7b73dbc4
Taylor Bartlett <taybart@gmail.com>
2015-04-08 20:50:31 -0600

    Updated UI

--- UI/test.js
+++ UI/test.js
@@ -20,95 +20,95 @@ $("#nav li").click(function() {
   $(this).addClass("active");
   doSwap();
   rotate('.dial',$(this));
-                     
+
 });
 
 function rotate(m,o) {
   var id = "#" + $(o).attr("id");
   var menu = $(m);
-  
+
   menu.removeClass('r0 lr1 lr2 rr1 rr2');
-  
-  if (id == "#email") {
+
+  if (id == "#calendar") {
     menu.addClass("lr2");
   }
   if (id == "#skype") {
     menu.addClass('lr1');
-    
+
   }
-  if (id == "#cloud") {
+  if (id == "#weather") {
     menu.addClass('r0');
   }
-  if (id == "#portfolio") {
+  if (id == "#settings") {
     menu.addClass('rr1');
   }
-  if (id == "#settings") {
-    menu.addClass('rr2');
-  }
-      
-  
+  // if (id == "#settings") {
+  //   menu.addClass('rr2');
+  // }
+
+
 }
 
 function swap(o) {
   var id = "#" + $(o).attr("id");
   var cimg = new String;
-  var burl = "http://grantcr.com/files/",
+  var burl = "icons/",
       ext = ".png"
   cimg = id + " img";
   console.log($(cimg).attr("src"));
-  
-  if (id == "#email") {
-    var nimg = burl + "iemailh" + ext;
+
+  if (id == "#calendar") {
+    var nimg = burl + "gcalh" + ext;
     $(cimg).attr("src",nimg);
   }
   if (id == "#skype") {
-    var nimg = burl + "photosh" + ext;
+    var nimg = burl + "helph" + ext;
     $(cimg).attr("src",nimg);
   }
-  if (id == "#cloud") {
-    var nimg = burl + "icloudh" + ext;
-    $(cimg).attr("src",nimg);
-  }
-  if (id == "#portfolio") {
-    var nimg = burl + "portfolioh" + ext;
+  if (id == "#weather") {
+    var nimg = burl + "weatherh" + ext;
     $(cimg).attr("src",nimg);
   }
   if (id == "#settings") {
     var nimg = burl + "settingsh" + ext;
     $(cimg).attr("src",nimg);
   }
-  
+  // if (id == "#settings") {
+  //   var nimg = burl + "weatherh" + ext;
+  //   $(cimg).attr("src",nimg);
+  // }
+
 }
 
 function swapBack(o) {
   var id = "#" + $(o).attr("id");
   var cimg = new String;
-  var burl = "http://grantcr.com/files/",
+  var burl = "icons/",
       ext = ".png"
   cimg = id + " img";
   console.log($(cimg).attr("src"));
-  
-  if (id == "#email") {
-    var nimg = burl + "iemail" + ext;
+
+  if (id == "#calendar") {
+    var nimg = burl + "gcal"+ ext;
     $(cimg).attr("src",nimg);
   }
   if (id == "#skype") {
-    var nimg = burl + "iphoto" + ext;
+    var nimg = burl + "help" + ext;
     $(cimg).attr("src",nimg);
   }
-  if (id == "#cloud") {
-    var nimg = burl + "icloud" + ext;
-    $(cimg).attr("src",nimg);
-  }
-  if (id == "#portfolio") {
-    var nimg = burl + "portfolio" + ext;
+  if (id == "#weather") {
+    var nimg = burl + "weather" + ext;
     $(cimg).attr("src",nimg);
   }
   if (id == "#settings") {
     var nimg = burl + "settings" + ext;
     $(cimg).attr("src",nimg);
   }
-  
+  // if (id == "#settings") {
+  //   var nimg = burl + "weatherb" + ext;
+  //   $(cimg).attr("src",nimg);
+  // }
+
 }
 });
 
@@ -116,7 +116,7 @@ function swapBack(o) {
 //card flip
 
 $(function(){
-document.querySelector("#email").onclick = function(e) {
+document.querySelector("#calendar").onclick = function(e) {
 
    e.preventDefault();
    document.querySelector('.cards').classList.toggle('flip');
@@ -132,18 +132,10 @@ document.querySelector("#skype").onclick = function(e) {
 });
 
 $(function(){
-document.querySelector('#cloud').onclick = function(e) {
+document.querySelector('#weather').onclick = function(e) {
 
    e.preventDefault();
    document.querySelector('.cards3').classList.toggle('flip3');
-}
-});
-
-$(function(){
-document.querySelector("#portfolio").onclick = function(e) {
-
-   e.preventDefault();
-   document.querySelector('.cards4').classList.toggle('flip4');
 }
 });
 
@@ -154,6 +146,14 @@ document.querySelector("#settings").onclick = function(e) {
    document.querySelector('.cards4').classList.toggle('flip4');
 }
 });
+
+// $(function(){
+// document.querySelector("#settings").onclick = function(e) {
+
+//    e.preventDefault();
+//    document.querySelector('.cards4').classList.toggle('flip4');
+// }
+// });
 
 
 
